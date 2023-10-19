2471272ad10ab279d1905e5b7fcc7f817a14b1c8
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-10-19 13:25:49 -0700

    inquirer prompt

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: 'What is your location?',
+      name: 'location',
+    },
+    {
+      type: 'input',
+      message: 'Enter your Bio',
+      name: 'bio',
+    },
+    {
+        type: 'input',
+        message: 'Enter your LinkedIn URL',
+        name: 'linku',
+      },
+      {
+        type: 'input',
+        message: 'Enter your GitHub URL',
+        name: 'gitu',
+      },
+  ])
+  .then((response) => {
+    console.log(response)
+
+  });
+  
+  fs.writeFile('index.html', process.argv[2], (err) =>
+  err ? console.error(err) : console.log('Success!')
+);
